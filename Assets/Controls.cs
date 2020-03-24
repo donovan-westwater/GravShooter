740e64e76464af7b3f3f712cff3c24cef78c715f
Donovan Westwater <donovan.westwater@gmail.com>
2020-03-23 20:11:44 -0400

    This is the first real commit. Has basic movment and aiming. Need to add controller input

---
+++ Assets/Controls.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    //Movement Variables
+    Vector3 grav = new Vector3(0, -1f, 0);
+    public Vector3 velo = new Vector2(0, 0);
+    public float speed = 5f;
+
+    //Aiming Variables
+    Vector2 aimDir;
+    Transform aimRet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimRet = this.transform.GetChild(0);
+        aimDir = aimRet.transform.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    //TODO: Rework the aim system to use 8 directional aim. turn off sprites except the direction aimed
+    //EX:
+    // \ | /
+    // -   -
+    // / | \
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 veloIN = new Vector3(h, 0, 0);
+        if (!Input.anyKey) velo = new Vector3(0, 0, 0);
+        if(v < 0) grav = new Vector3(0, -1f, 0);
+        else if(v > 0) grav = new Vector3(0, 1f, 0);
+        velo = ((veloIN.normalized+ grav.normalized)*speed);
+        this.transform.Translate(velo * Time.deltaTime);
+
+        //Aiming Contorls
+        Vector3 mouseVect = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y);
+        Vector3 currentDir = new Vector3(aimRet.transform.position.x - transform.position.x, aimRet.transform.position.y - transform.position.y);
+        float angleTemp = Mathf.Acos(Vector3.Dot(currentDir, mouseVect) / (currentDir.magnitude * mouseVect.magnitude)); //Is in Radians!
+        if (Vector3.Cross(mouseVect.normalized, currentDir.normalized).z < 0 && angleTemp > Mathf.Abs(0.01f))
+        {
+            aimRet.transform.RotateAround(transform.position, new Vector3(0, 0, 1), angleTemp*Mathf.Rad2Deg);
+        }
+        else if (Vector3.Cross(mouseVect.normalized, currentDir.normalized).z > 0 && angleTemp > Mathf.Abs(0.01f))
+        {
+            aimRet.transform.RotateAround(transform.position, new Vector3(0, 0, 1), -angleTemp*Mathf.Rad2Deg);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        ContactPoint2D[] points = new ContactPoint2D[500];
+        collision.GetContacts(points);
+        ContactPoint2D? prevP = null;
+        foreach (ContactPoint2D p in points)
+        {
+            if (p.collider == null) continue;
+            if (prevP != null && Mathf.Abs(prevP.Value.point.y - p.point.y) > 0.1f)
+            {
+                return;
+            }
+            prevP = p;
+        }
+        //velo = new Vector3(0, 0, 0);
+        grav = new Vector3(0, 0, 0);
+    }
+}
---
+++ Assets/ControlsProto.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsProto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D phy;
+    public Vector3 velo = new Vector2(0,0);
+    public float speed = 5f;
+    void Start()
+    {
+        phy = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    //Fix gravity problem!!!
+    private void Update()
+    {
+       // float h = Input.GetAxis("Horizontal");
+       // velo = new Vector3(h, 0, 0);
+        //velo = velo.normalized * speed * Time.deltaTime;
+        //phy.MovePosition(phy.transform.position + velo);
+        //phy.AddForce(velo);
+        if (!Input.anyKey)
+        {
+            velo = new Vector3(0, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            phy.gravityScale = -phy.gravityScale;
+        }
+        //if (Mathf.Abs(velo.magnitude) > 8f) velo = velo.normalized * 8f;
+       // this.transform.position += velo;
+    }
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        velo = new Vector3(h, 0, 0);
+        velo = velo.normalized * speed;
+        //if (Mathf.Abs(velo.magnitude) > 8f) velo = velo.normalized * 8f;
+        phy.AddForce(velo);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ContactPoint2D[] points = new ContactPoint2D[500];
+        collision.GetContacts(points);
+        ContactPoint2D? prevP = null;
+        foreach (ContactPoint2D p in points)
+        {
+            if (prevP != null && prevP.Value.point.y != p.point.y)
+            {
+                velo = new Vector3(0, 0, 0);
+                return;
+            }
+            prevP = p;
+        }
+    }
+}
+
